a5005b37ec48c696d2f8416c14d37d201d0de710
tcisxy <tc1253727161@163.com>
2018-04-26 18:02:56 +0800

    Create EmployeeService.java

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getEmpList();
+
+    void save(Employee employee);
+
+    List<Employee> checkAccount(String account,String password);
+
+    void edit(Employee employee);
+
+    void delete(Long id);
+
+    Employee getEmpById(Long id);
+}
