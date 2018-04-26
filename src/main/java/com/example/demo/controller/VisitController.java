81093f79bc52c96c6de2e1b4aea5d2aa1dc08f18
tcisxy <tc1253727161@163.com>
2018-04-26 17:48:32 +0800

    Create VisitController.java

---
+++ src/main/java/com/example/demo/controller/VisitController.java
@@ -0,0 +1,70 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Doctor;
+import com.example.demo.entity.User;
+import com.example.demo.entity.Visit;
+import com.example.demo.service.DoctorService;
+import com.example.demo.service.UserService;
+import com.example.demo.service.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class VisitController {
+    @Resource
+    VisitService visitService;
+    @Resource
+    DoctorService doctorService;
+    @Resource
+    UserService userService;
+
+    @RequestMapping("/list/visit")
+    public ModelAndView list(ModelAndView modelAndView) {
+        List<Visit> visits = visitService.getUserVisitList();
+        modelAndView.addObject("visits", visits);
+        modelAndView.setViewName("visit/list.html");
+        return modelAndView;
+    }
+
+    @RequestMapping("/toAdd/visit")
+    public ModelAndView toAdd(ModelAndView modelAndView) {
+        List<Doctor> doctors = doctorService.getDoctorList();
+        modelAndView.addObject("doctors", doctors);
+        modelAndView.setViewName("/visit/addVisit.html");
+        return modelAndView;
+    }
+
+    @RequestMapping("/add/visit")
+    public ModelAndView add(ModelAndView modelAndView, Visit visit) {
+        User user = userService.getUserByPhone(visit.getPhone());
+        if(user == null) {
+            String message = "此用户不存在！";
+            modelAndView.addObject("message",message);
+            modelAndView.setViewName("visit/list.html");
+        }
+        visitService.save(visit);
+        modelAndView.setViewName("redirect:/list/visit");
+        return modelAndView;
+    }
+
+    @RequestMapping("/toEdit/visit")
+    public ModelAndView toEdit(ModelAndView modelAndView, Long id) {
+        Visit visit = visitService.findById(id);
+        modelAndView.addObject("visit",visit);
+        modelAndView.setViewName("visit/editVisit.html");
+        return modelAndView;
+    }
+
+    @RequestMapping("/edit/visit")
+    public ModelAndView edit(ModelAndView modelAndView, Visit visit) {
+        visitService.edit(visit);
+        modelAndView.setViewName("redirect:/list/visit");
+        return modelAndView;
+    }
+}
