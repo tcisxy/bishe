f5253dc3229c598da726d58a811cc0480369cade
tcisxy <tc1253727161@163.com>
2018-04-26 17:54:18 +0800

    Create Sidebar.java

---
+++ src/main/java/com/example/demo/param/Sidebar.java
@@ -0,0 +1,40 @@
+package com.example.demo.param;
+
+public class SideBar {
+    private String name;
+    private String url;
+    private String icon;
+    private boolean show;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public boolean isShow() {
+        return show;
+    }
+
+    public void setShow(boolean show) {
+        this.show = show;
+    }
+}
