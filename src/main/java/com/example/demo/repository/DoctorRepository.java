a6681dfd7f9d3d5ef886797cf0fa5e12bac7a325
tcisxy <tc1253727161@163.com>
2018-04-26 17:57:25 +0800

    Create DoctorRepository.java

---
+++ src/main/java/com/example/demo/repository/DoctorRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+    Doctor findById(long id);
+
+    @Query("update Doctor set name = ?2,age = ?3,sex = ?4,phone = ?5,description = ?6,position = ?7,price = ?8 where id = ?1")
+    @Modifying
+    @Transactional
+    void editDoctor(long id, String name, int age, int sex, String phone, String description, String position, long price);
+}
