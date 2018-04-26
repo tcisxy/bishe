7e7fc57573fa36f28d38540aacaf0e84ceac92f1
tcisxy <tc1253727161@163.com>
2018-04-26 17:59:11 +0800

    Create UserRepository.java

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByPhone(String phone);
+
+    User findById(long id);
+
+    @Query("update User set name = ?2,age = ?3,sex = ?4,phone = ?5 where id = ?1")
+    @Modifying
+    @Transactional
+    void editUser(long id,String name,int age,int sex,String phone);
+}
