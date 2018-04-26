528a00202c18211a5b70556c3476ec4a9826692b
tcisxy <tc1253727161@163.com>
2018-04-26 17:50:47 +0800

    Create Doctor.java

---
+++ src/main/java/com/example/demo/entity/Doctor.java
@@ -0,0 +1,103 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "t_doctor")
+public class Doctor implements Serializable {
+    private static final long serialVersionUID = 3L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private Integer age;
+    @Column(name = "sex")
+    private Integer sex;
+    @Column(name = "position")
+    private String position;
+    @Column(name = "deal_num")
+    private Integer dealNum;
+    @Column(name = "price")
+    private Long price;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "description")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getDealNum() {
+        return dealNum;
+    }
+
+    public void setDealNum(Integer dealNum) {
+        this.dealNum = dealNum;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
