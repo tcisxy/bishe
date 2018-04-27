7f0142eeb51185b0dc4895165870dd004212aa5e
tcisxy <tc1253727161@163.com>
2018-04-27 17:30:32 +0800

    Update VisitRepository.java

--- src/main/java/com/example/demo/repository/VisitRepository.java
+++ src/main/java/com/example/demo/repository/VisitRepository.java
@@ -3,13 +3,15 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
+import java.util.List;
 
-public interface VisitRepository extends JpaRepository<Visit,Long> {
+public interface VisitRepository extends JpaRepository<Visit,Long>,JpaSpecificationExecutor<Visit> {
     Visit findById(long id);
 
     @Query("update Visit set time = ?2,description = ?3,suggestion = ?4,fee = ?5 where id = ?1")
@@ -22,4 +24,11 @@ public interface VisitRepository extends JpaRepository<Visit,Long> {
 
     @Query
     int countByDoctorId(long doctorId);
+
+    @Query("select count(Visit) from Visit where time between ?1 and ?2")
+    long countToday(Timestamp start, Timestamp end);
+
+    List<Visit> findAllByOrderByTimeDesc();
+
+    List<Visit> findByUserIdOrderByTimeDesc(long userId);
 }
