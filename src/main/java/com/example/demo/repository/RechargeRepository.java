6cc671b3b29a86df42646d8bfe2ccee307755c93
tcisxy <tc1253727161@163.com>
2018-04-27 09:59:13 +0800

    Update RechargeRepository.java

--- src/main/java/com/example/demo/repository/RechargeRepository.java
+++ src/main/java/com/example/demo/repository/RechargeRepository.java
@@ -1,12 +1,25 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Employee;
+import com.example.demo.entity.Recharge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.util.List;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-    List<Employee> findByAccountAndPassword(String account,String password);
+public interface RechargeRepository extends JpaRepository<Recharge,Long> {
+    Recharge findById(long id);
 
-    Employee findById(long id);
+    @Query("select sum(money) from Recharge where userId = ?1")
+    Long sumMoneyByUserId(long userId);
+
+    List<Recharge> findByUserId(long userId);
+
+    @Query("select r from Recharge r where r.time between ?1 and ?2")
+    List<Recharge> findByStartTimeAndEndTime(Timestamp startTime, Timestamp endTime);
+
+    @Query("select r from Recharge r where r.userId = ?3 and r.time between ?1 and ?2")
+    List<Recharge> findByStartTimeAndEndTimeAndUserId(Timestamp startTime, Timestamp endTime, long userId);
 }
