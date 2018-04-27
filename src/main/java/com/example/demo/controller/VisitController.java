28a6b3dd352240e01d4142c832d83e7f824fd132
tcisxy <tc1253727161@163.com>
2018-04-27 17:35:07 +0800

    Update VisitController.java

--- src/main/java/com/example/demo/controller/VisitController.java
+++ src/main/java/com/example/demo/controller/VisitController.java
@@ -1,11 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.component.PayComponent;
+import com.example.demo.entity.Consume;
 import com.example.demo.entity.Doctor;
 import com.example.demo.entity.User;
 import com.example.demo.entity.Visit;
+import com.example.demo.param.QueryParam;
+import com.example.demo.repository.ConsumeRepository;
+import com.example.demo.service.ConsumeService;
 import com.example.demo.service.DoctorService;
 import com.example.demo.service.UserService;
 import com.example.demo.service.VisitService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +30,10 @@ public class VisitController {
     DoctorService doctorService;
     @Resource
     UserService userService;
+    @Resource
+    ConsumeService consumeService;
+    @Autowired
+    PayComponent payComponent;
 
     @RequestMapping("/list/visit")
     public ModelAndView list(ModelAndView modelAndView) {
@@ -65,6 +76,38 @@ public class VisitController {
     public ModelAndView edit(ModelAndView modelAndView, Visit visit) {
         visitService.edit(visit);
         modelAndView.setViewName("redirect:/list/visit");
+        return modelAndView;
+    }
+
+    @RequestMapping("/toDeal/visit")
+    public ModelAndView toDeal(ModelAndView modelAndView, Long id) {
+        Visit visit = visitService.findById(id);
+        modelAndView.addObject("visit",visit);
+        modelAndView.addObject("pays",payComponent.getType());
+        modelAndView.setViewName("visit/dealVisit.html");
+        return modelAndView;
+    }
+
+    @RequestMapping("/deal/visit")
+    public ModelAndView deal(ModelAndView modelAndView, Consume consume) {
+        Consume newConsume = new Consume();
+        Visit visit = visitService.findById(consume.getId());
+        newConsume.setType(consume.getType());
+        newConsume.setMoney(consume.getMoney());
+        newConsume.setUserId(visit.getUserId());
+        newConsume.setConsumeStatus(0);
+        visit.setStatus(1);
+        visit.setConsumeId(consumeService.save(newConsume).getId());
+        visitService.save(visit);
+        modelAndView.setViewName("redirect:/list/visit");
+        return modelAndView;
+    }
+
+    @RequestMapping("/query/visit")
+    public ModelAndView query(ModelAndView modelAndView, QueryParam queryParam) {
+        modelAndView.addObject("visits",visitService.queryByParam(queryParam));
+        modelAndView.setViewName("visit/list.html");
+        modelAndView.addObject("phone",queryParam.getPhone());
         return modelAndView;
     }
 }
