eb9dc409a6ce07636f15b90512bc4f53f150bdd0
tcisxy <tc1253727161@163.com>
2018-04-27 17:36:20 +0800

    Update IndexController.java

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -2,24 +2,22 @@ package com.example.demo.controller;
 
 import com.example.demo.component.AuthComponent;
 import com.example.demo.component.WebSecurityConfig;
+import com.example.demo.entity.Consume;
 import com.example.demo.entity.Department;
 import com.example.demo.entity.Employee;
+import com.example.demo.param.IndexData;
 import com.example.demo.param.SideBar;
-import com.example.demo.service.DepartmentService;
-import com.example.demo.service.EmployeeService;
+import com.example.demo.service.*;
+import com.example.demo.util.IndexUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 public class IndexController {
@@ -31,9 +29,23 @@ public class IndexController {
     EmployeeService employeeService;
     @Resource
     DepartmentService departmentService;
+    @Resource
+    UserService userService;
+    @Resource
+    DoctorService doctorService;
+    @Resource
+    AppointmentService appointmentService;
+    @Resource
+    VisitService visitService;
+    @Resource
+    ConsumeService consumeService;
 
     @RequestMapping("/index")
     public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.addObject("user",userService.count());
+        modelAndView.addObject("doctor",doctorService.count());
+        modelAndView.addObject("appoint",appointmentService.count());
+        modelAndView.addObject("visit",visitService.count());
         modelAndView.setViewName("index.html");
         return modelAndView;
     }
@@ -79,7 +91,7 @@ public class IndexController {
         session.setAttribute(WebSecurityConfig.SESSION_AUTH,sideBars);
         session.setAttribute(WebSecurityConfig.SESSION_INDEX,indexs);
 
-        modelAndView.setViewName("index.html");
+        modelAndView.setViewName("redirect:/index");
         return modelAndView;
     }
 
@@ -92,6 +104,21 @@ public class IndexController {
 
         modelAndView.setViewName("redirect:/login");
         return modelAndView;
+    }
+
+    @RequestMapping("/data/con")
+    public IndexData consumeData() {
+        Map<String,Long> map = new HashMap<>();
+        List<Consume> consumes = consumeService.getConsumeListOrderByTime();
+        for(Consume consume : consumes) {
+            String time = new SimpleDateFormat("yyyy-MM-dd").format(consume.getTime());
+            if(map.get(time) == null) {
+                map.put(time,consume.getMoney());
+            }else {
+                map.put(time,map.get(time) + consume.getMoney());
+            }
+        }
+        return IndexUtil.getIndexData(map);
     }
 }
 
