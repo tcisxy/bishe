1fb3adab047215a6e2a884f4ddb001a37afb05c8
tcisxy <tc1253727161@163.com>
2018-04-27 17:31:45 +0800

    Update AppointmentRepository.java

--- src/main/java/com/example/demo/repository/AppointmentRepository.java
+++ src/main/java/com/example/demo/repository/AppointmentRepository.java
@@ -2,13 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.util.List;
 
-public interface AppointmentRepository extends JpaRepository<Appointment,Long>{
+public interface AppointmentRepository extends JpaRepository<Appointment,Long>,JpaSpecificationExecutor<Appointment> {
     Appointment findById(long id);
 
     List<Appointment> findByUserId(long id);
@@ -17,4 +19,7 @@ public interface AppointmentRepository extends JpaRepository<Appointment,Long>{
     @Modifying
     @Transactional
     void edit(Long id,Integer status);
+
+    @Query("select count(Appointment ) from Appointment where time between ?1 and ?2")
+    long countToday(Timestamp start, Timestamp end);
 }
