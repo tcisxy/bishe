60f9d9a5e4082aede59924b1b6b8af9546a748fc
tcisxy <tc1253727161@163.com>
2018-04-27 17:27:48 +0800

    Update AppointmentService.java

--- src/main/java/com/example/demo/service/AppointmentService.java
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -1,17 +1,24 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Appointment;
+import com.example.demo.param.QueryParam;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface AppointmentService {
     List<Appointment> getAppointList();
 
-    List<Appointment> getAppointmentByPhone(String phone);
+    List<Appointment> getAppointmentByParam(QueryParam queryParam);
 
     Appointment getAppointmentById(long id);
 
     void save(Appointment appointment);
 
     void edit(Appointment appointment);
+
+    long count();
+
+    @Transactional
+    boolean queue(long id);
 }
