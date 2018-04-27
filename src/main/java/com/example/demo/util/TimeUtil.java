93a2197fb218cf906f944c325f9b6bdba1375419
tcisxy <tc1253727161@163.com>
2018-04-27 17:25:09 +0800

    Create TimeUtil.java

---
+++ src/main/java/com/example/demo/util/TimeUtil.java
@@ -0,0 +1,24 @@
+package com.example.demo.util;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+public class TimeUtil {
+    public static Timestamp getTodayStartTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY,0);
+        calendar.set(Calendar.MINUTE,0);
+        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.MILLISECOND,0);
+        return new Timestamp(calendar.getTimeInMillis());
+    }
+
+    public static Timestamp getTodayEndTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY,23);
+        calendar.set(Calendar.MINUTE,59);
+        calendar.set(Calendar.SECOND,59);
+        calendar.set(Calendar.MILLISECOND,999);
+        return new Timestamp(calendar.getTimeInMillis());
+    }
+}
