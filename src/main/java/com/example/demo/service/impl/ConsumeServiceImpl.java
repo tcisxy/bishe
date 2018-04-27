d37560ddc3f3b7a31be461c572021f9b654b962b
tcisxy <tc1253727161@163.com>
2018-04-27 17:29:47 +0800

    Update ConsumeServiceImpl.java

--- src/main/java/com/example/demo/service/impl/ConsumeServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ConsumeServiceImpl.java
@@ -3,13 +3,24 @@ package com.example.demo.service.impl;
 import com.example.demo.component.PayComponent;
 import com.example.demo.entity.Consume;
 import com.example.demo.entity.User;
+import com.example.demo.entity.VipLevel;
+import com.example.demo.param.QueryParam;
 import com.example.demo.repository.ConsumeRepository;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.VipLevelRepository;
 import com.example.demo.service.ConsumeService;
+import com.example.demo.util.IndexUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,11 +34,13 @@ public class ConsumeServiceImpl implements ConsumeService {
     private UserRepository userRepository;
     @Autowired
     private PayComponent payComponent;
+    @Autowired
+    private VipLevelRepository vipLevelRepository;
 
     @Override
     public List<Consume> getConsumeList() {
-        List<Consume> list = consumeRepository.findAll();
-        for(Consume consume : list) {
+        List<Consume> list = consumeRepository.findAllByOrderByTimeDesc();
+        for (Consume consume : list) {
             User user = userRepository.findById(consume.getUserId());
             consume.setUserName(user.getName());
             consume.setPhone(user.getPhone());
@@ -44,72 +57,76 @@ public class ConsumeServiceImpl implements ConsumeService {
     }
 
     @Override
-    public List<Consume> getConsumeListByParam(Consume consume) {
-        List<Consume> consumes = new ArrayList<>();
-        if(!StringUtils.isEmpty(consume.getPhone())) {
-            User user = userRepository.findByPhone(consume.getPhone());
-            if(user != null) {
-                if(consume.getStartTime() != null) {
-                    if(consume.getEndTime() != null) {
-                        consumes = consumeRepository.findByStartTimeAndEndTimeAndUserId(consume.getStartTime(),consume.getEndTime(),user.getId());
-                    }else{
-                        consumes = consumeRepository.findByStartTimeAndEndTimeAndUserId(consume.getStartTime(), new Timestamp(System.currentTimeMillis()), user.getId());
-                    }
-                }else {
-                    if(consume.getEndTime() != null) {
-                        consumes = consumeRepository.findByStartTimeAndEndTimeAndUserId(new Timestamp(0L),consume.getEndTime(),user.getId());
-                    }else{
-                        consumes = consumeRepository.findByUserId(user.getId());
-                    }
-                }
-            }
-        }else {
-            if(consume.getStartTime() != null) {
-                if(consume.getEndTime() != null) {
-                    consumes = consumeRepository.findByStartTimeAndEndTime(consume.getStartTime(),consume.getEndTime());
-                }else {
-                    consumes = consumeRepository.findByStartTimeAndEndTime(consume.getStartTime(),new Timestamp(System.currentTimeMillis()));
-                }
-            }else{
-                if(consume.getEndTime() != null) {
-                    consumes = consumeRepository.findByStartTimeAndEndTime(new Timestamp(0L),consume.getEndTime());
-                }
-            }
+    public List<Consume> getConsumeListByParam(QueryParam queryParam) {
+        List<Consume> consumes = consumeRepository.findAll(getWhereClause(queryParam.getPhone(),queryParam.getStartTime(),queryParam.getEndTime()),new Sort(Sort.Direction.DESC,"time"));
+        for(Consume _consume : consumes) {
+            User user = userRepository.findById(_consume.getUserId());
+            _consume.setPhone(user.getPhone());
+            _consume.setUserName(user.getName());
+            _consume.setPayName(payComponent.getType().get(_consume.getType()));
         }
-
-        for (Consume _consume : consumes) {
-            User _user = userRepository.findById(_consume.getUserId());
-            _consume.setPhone(_user.getPhone());
-            _consume.setUserName(_user.getName());
-        }
-
         return consumes;
     }
 
     @Override
-    public void save(Consume consume) {
-        if(consume.getConsumeStatus() == null)
+    public Consume save(Consume consume) {
+        if (consume.getConsumeStatus() == null)
             consume.setConsumeStatus(0);
-        User user = userRepository.findByPhone(consume.getPhone());
-        consume.setUserId(user.getId());
-        consume.setTime(new Timestamp(System.currentTimeMillis()));
+        User user;
+        if(consume.getPhone() != null) {
+            user = userRepository.findByPhone(consume.getPhone());
+            consume.setUserId(user.getId());
+        }else {
+            user = userRepository.findById(consume.getUserId());
+        }
+
+        if (consume.getTime() == null)
+            consume.setTime(new Timestamp(System.currentTimeMillis()));
         user.setTotalConsume(user.getTotalConsume() + consume.getMoney());
-        user.setCurMoney(user.getCurMoney() - consume.getMoney());
-        consumeRepository.save(consume);
+        user.setVipLevel(IndexUtil.getVipLevel(vipLevelRepository.findAll(),user.getTotalConsume()));
+        return consumeRepository.save(consume);
     }
 
     @Override
     public void edit(Consume consume) {
-        consumeRepository.editConsumeById(consume.getId(),consume.getConsumeStatus());
+        consumeRepository.editConsumeById(consume.getId(), consume.getConsumeStatus());
     }
 
     @Override
     public void delete(long id) {
         Consume consume = consumeRepository.findById(id);
         User user = userRepository.findById(consume.getUserId());
-        user.setCurMoney(user.getCurMoney() + consume.getMoney());
         user.setTotalConsume(user.getTotalConsume() - consume.getMoney());
+        user.setVipLevel(IndexUtil.getVipLevel(vipLevelRepository.findAll(),user.getTotalConsume()));
         consumeRepository.deleteById(id);
     }
 
+    @Override
+    public List<Consume> getConsumeListOrderByTime() {
+        return consumeRepository.findAllByOrderByTime();
+    }
+
+    private Specification<Consume> getWhereClause(final String phone,final Timestamp startTime,final Timestamp endTime) {
+        return new Specification<Consume>() {
+            @Nullable
+            @Override
+            public Predicate toPredicate(Root<Consume> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                Predicate predicate = criteriaBuilder.conjunction();
+                if(phone != null){
+                    User user = userRepository.findByPhone(phone);
+                    if(user != null)
+                        predicate.getExpressions().add(criteriaBuilder.equal(root.<Long>get("userId"),user.getId()));
+                }
+
+                if(startTime != null && endTime != null)
+                    predicate.getExpressions().add(criteriaBuilder.between(root.<Timestamp>get("time"),startTime,endTime));
+                else if(startTime != null && endTime == null)
+                    predicate.getExpressions().add(criteriaBuilder.greaterThanOrEqualTo(root.<Timestamp>get("time"),startTime));
+                else if(startTime == null && endTime != null)
+                    predicate.getExpressions().add(criteriaBuilder.lessThanOrEqualTo(root.<Timestamp>get("time"),endTime));
+
+                return predicate;
+            }
+        };
+    }
 }
