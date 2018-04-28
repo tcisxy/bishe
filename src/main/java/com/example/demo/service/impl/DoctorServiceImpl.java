7a5689fceffb58154981781b1b5dd6a36ba75e04
tcisxy <tc1253727161@163.com>
2018-04-28 17:52:23 +0800

    Update DoctorServiceImpl.java

--- src/main/java/com/example/demo/service/impl/DoctorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DoctorServiceImpl.java
@@ -1,12 +1,23 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.Doctor;
+import com.example.demo.param.QueryParam;
+import com.example.demo.repository.AppointmentRepository;
 import com.example.demo.repository.DoctorRepository;
 import com.example.demo.repository.VisitRepository;
 import com.example.demo.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,6 +27,8 @@ public class DoctorServiceImpl implements DoctorService {
     private DoctorRepository doctorRepository;
     @Autowired
     private VisitRepository visitRepository;
+    @Autowired
+    private AppointmentRepository appointmentRepository;
 
     @Override
     public List<Doctor> getDoctorList() {
@@ -70,4 +83,38 @@ public class DoctorServiceImpl implements DoctorService {
     public long count() {
         return doctorRepository.count();
     }
+
+    @Override
+    public List<Doctor> getAvailableDoctorList(Timestamp startTime, Timestamp endTime) {
+        List<Long> appointedDoctors = appointmentRepository.findAppointedDoctor(startTime,endTime);
+        List<Doctor> availableDoctors = new ArrayList<>();
+        for(Doctor doctor : doctorRepository.findAll()) {
+            if(!appointedDoctors.contains(doctor.getId())) {
+                availableDoctors.add(doctor);
+            }
+        }
+        return availableDoctors;
+    }
+
+    @Override
+    public List<Doctor> getDoctorListByParam(QueryParam queryParam) {
+        return doctorRepository.findAll(getWhereClause(queryParam.getName(),queryParam.getPhone()));
+    }
+
+    private Specification<Doctor> getWhereClause(final String name, final String phone) {
+        return new Specification<Doctor>() {
+            @Nullable
+            @Override
+            public Predicate toPredicate(Root<Doctor> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                Predicate predicate = criteriaBuilder.conjunction();
+                if(!StringUtils.isEmpty(name))
+                    predicate.getExpressions().add(criteriaBuilder.equal(root.<String>get("name"),name));
+                if(!StringUtils.isEmpty(phone))
+                    predicate.getExpressions().add(criteriaBuilder.equal(root.<String>get("phone"),phone));
+                return predicate;
+            }
+        };
+    }
+
+
 }
