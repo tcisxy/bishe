ad71bc3e0f347800ebfe736634d3a9c68155255c
tcisxy <tc1253727161@163.com>
2018-04-28 17:57:17 +0800

    Update ConsumeController.java

--- src/main/java/com/example/demo/controller/ConsumeController.java
+++ src/main/java/com/example/demo/controller/ConsumeController.java
@@ -7,13 +7,13 @@ import com.example.demo.param.QueryParam;
 import com.example.demo.service.ConsumeService;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -29,6 +29,7 @@ public class ConsumeController {
     public ModelAndView list(ModelAndView modelAndView) {
         List<Consume> consumes = consumeService.getConsumeList();
         modelAndView.addObject("consumes", consumes);
+        modelAndView.addObject("sum",consumeService.sumMoneyByParam(new QueryParam()));
         modelAndView.setViewName("consume/list.html");
         return modelAndView;
     }
@@ -72,9 +73,44 @@ public class ConsumeController {
     @RequestMapping("/query/consume")
     public ModelAndView query(ModelAndView modelAndView, QueryParam queryParam) {
         List<Consume> consumes = consumeService.getConsumeListByParam(queryParam);
+        modelAndView.addObject("name",queryParam.getName());
         modelAndView.addObject("phone", queryParam.getPhone());
+        modelAndView.addObject("startTime", queryParam.getStartTime());
+        modelAndView.addObject("endTime",queryParam.getEndTime());
         modelAndView.addObject("consumes",consumes);
+        modelAndView.addObject("sum",consumeService.sumMoneyByParam(queryParam));
         modelAndView.setViewName("consume/list.html");
+        return modelAndView;
+    }
+
+    class ConsumeData {
+        public Timestamp time;
+        public Long money;
+    }
+
+    @RequestMapping("/data/user/consume")
+    public List<ConsumeData> dataUserConsume(long id) {
+        List<ConsumeData> consumeDatas = new ArrayList<>();
+        User user = userService.getUserById(id);
+        if(user == null) {
+            return consumeDatas;
+        }
+        QueryParam param = new QueryParam();
+        param.setName(user.getName());
+        param.setPhone(user.getPhone());
+        for(Consume consume : consumeService.getConsumeListByParam(param)) {
+            ConsumeData consumeData = new ConsumeData();
+            consumeData.money = consume.getMoney();
+            consumeData.time = consume.getTime();
+            consumeDatas.add(consumeData);
+        }
+        return consumeDatas;
+    }
+
+    @RequestMapping("/user/consume")
+    public ModelAndView userConsume(ModelAndView modelAndView,long id) {
+        modelAndView.addObject("id",id);
+        modelAndView.setViewName("consume/userConsume.html");
         return modelAndView;
     }
 
