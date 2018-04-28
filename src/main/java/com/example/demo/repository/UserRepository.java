d87c52c31fa2b4340df9b3c68980152861991341
tcisxy <tc1253727161@163.com>
2018-04-28 17:53:38 +0800

    Update UserRepository.java

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,13 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long>,JpaSpecificationExecutor<User> {
     User findByPhone(String phone);
 
     User findById(long id);
