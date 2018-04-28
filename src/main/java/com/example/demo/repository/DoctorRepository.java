501c94c50d6527439ef6e97a66951624eaf9fccb
tcisxy <tc1253727161@163.com>
2018-04-28 17:54:10 +0800

    Update DoctorRepository.java

--- src/main/java/com/example/demo/repository/DoctorRepository.java
+++ src/main/java/com/example/demo/repository/DoctorRepository.java
@@ -2,11 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+public interface DoctorRepository extends JpaRepository<Doctor,Long>,JpaSpecificationExecutor<Doctor> {
     Doctor findById(long id);
 
     @Query("update Doctor set name = ?2,age = ?3,sex = ?4,phone = ?5,description = ?6,position = ?7,price = ?8 where id = ?1")
