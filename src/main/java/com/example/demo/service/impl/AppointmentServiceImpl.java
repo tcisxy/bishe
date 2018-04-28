3c08dca3b1748d08699e3cdb800918e962cac9a4
tcisxy <tc1253727161@163.com>
2018-04-28 17:52:57 +0800

    Update AppointmentServiceImpl.java

--- src/main/java/com/example/demo/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AppointmentServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -36,6 +37,8 @@ public class AppointmentServiceImpl implements AppointmentService {
     private UserRepository userRepository;
     @Autowired
     private VisitRepository visitRepository;
+    @Autowired
+    private UserServiceImpl userServiceImpl;
 
     @Override
     public List<Appointment> getAppointList() {
@@ -50,7 +53,7 @@ public class AppointmentServiceImpl implements AppointmentService {
 
     @Override
     public List<Appointment> getAppointmentByParam(QueryParam queryParam) {
-        List<Appointment> appointments = appointmentRepository.findAll(getWhereClause(queryParam.getPhone(), queryParam.getStartTime(), queryParam.getEndTime()), new Sort(Sort.Direction.DESC, "time"));
+        List<Appointment> appointments = appointmentRepository.findAll(getWhereClause(queryParam.getName().trim(), queryParam.getPhone().trim(),queryParam.getDoctorId(), queryParam.getStartTime(), queryParam.getEndTime()), new Sort(Sort.Direction.DESC, "time"));
         for (Appointment _appointment : appointments) {
             _appointment.setUserName(userRepository.findById(_appointment.getUserId()).get().getName());
             _appointment.setPhone(userRepository.findById(_appointment.getUserId()).get().getPhone());
@@ -69,7 +72,7 @@ public class AppointmentServiceImpl implements AppointmentService {
     }
 
     @Override
-    public void save(Appointment appointment) {
+    public boolean save(Appointment appointment) {
         if (appointment.getAppointStatus() == null) {
             appointment.setAppointStatus(0);
         }
@@ -80,7 +83,10 @@ public class AppointmentServiceImpl implements AppointmentService {
             User user = userRepository.findByPhone(appointment.getPhone());
             appointment.setUserId(user.getId());
         }
+        if(appointmentRepository.findAppointedDoctor(appointment.getTime(),new Timestamp(appointment.getTime().getTime() + 59 * 60 * 1000)).contains(appointment.getDoctorId()))
+            return false;
         appointmentRepository.save(appointment);
+        return true;
     }
 
     @Override
@@ -112,25 +118,33 @@ public class AppointmentServiceImpl implements AppointmentService {
         return true;
     }
 
-    private Specification<Appointment> getWhereClause(final String phone, final Timestamp startTime, final Timestamp endTime) {
+    private Specification<Appointment> getWhereClause(final String name, final String phone,final Long doctorId, final Timestamp startTime, final Timestamp endTime) {
         return new Specification<Appointment>() {
             @Nullable
             @Override
             public Predicate toPredicate(Root<Appointment> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
                 Predicate predicate = criteriaBuilder.conjunction();
-                if (phone != null) {
-                    User user = userRepository.findByPhone(phone);
-                    if (user != null)
-                        predicate.getExpressions().add(criteriaBuilder.equal(root.<Long>get("userId"), user.getId()));
+                if(!StringUtils.isEmpty(name) || !StringUtils.isEmpty(phone)) {
+                    List<User> users = userRepository.findAll(userServiceImpl.getWhereClause(name,phone));
+                    if(users.size() == 0) {
+                        predicate.getExpressions().add(criteriaBuilder.equal(root.<Long>get("userId"),-1));
+                    }else {
+                        List<Long> userIds = new ArrayList<>();
+                        for(User user : users) {
+                            userIds.add(user.getId());
+                        }
+                        predicate.getExpressions().add(criteriaBuilder.and(root.<Long>get("userId").in(userIds)));
+                    }
                 }
-
+                if(doctorId != null) {
+                    predicate.getExpressions().add(criteriaBuilder.equal(root.<Timestamp>get("doctorId"), doctorId));
+                }
                 if (startTime != null && endTime != null)
                     predicate.getExpressions().add(criteriaBuilder.between(root.<Timestamp>get("time"), startTime, endTime));
                 else if (startTime != null && endTime == null)
                     predicate.getExpressions().add(criteriaBuilder.greaterThanOrEqualTo(root.<Timestamp>get("time"), startTime));
                 else if (startTime == null && endTime != null)
                     predicate.getExpressions().add(criteriaBuilder.lessThanOrEqualTo(root.<Timestamp>get("time"), endTime));
-
                 return predicate;
             }
         };
